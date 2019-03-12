19dfb9a7188b6c03a7a0c181119061d667ba2421
Marcus Vinícius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2019-03-12 20:56:18 -0300

    Mudança na variável do print e adição de float

--- exercicio1.php
+++ exercicio1.php
@@ -1,8 +1,8 @@
 <?php
 
 print "Digite um valor: ";
-$valor1 = fgets (STDIN);
+$valor1 = (float)fgets (STDIN);
 
 $valor1 = $valor1*1000;
 
-print "O valor convertido em milímetros será de: $valor \n\n";
+print "O valor convertido em milímetros será de: $valor1 \n\n";
