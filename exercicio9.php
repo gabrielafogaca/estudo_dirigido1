e35bcd91f311c1503e32f422d033f622ebdb7ed9
gabrielafogaca <48135811+gabrielafogaca@users.noreply.github.com>
2019-03-15 16:41:05 -0300

    Update exercicio9.php

--- exercicio9.php
+++ exercicio9.php
@@ -1,10 +1,7 @@
 <?php
-
 print "Digite o tamanho em metros quadrados da área a ser pintada:";
 $areapintada = fgets(STDIN);
-
     $area_folga = $areapintada+(($areapintada*10)/100);
-
     $quantidade_lata = ceil($area_folga/108);  
     $preco_lata = $quantidade_lata*80; 
 
@@ -17,19 +14,20 @@ print "ou \n";
     $preco_galao = $quantidade_galao*25;
 
 print "Você precisará de $quantidade_galao galão(ões) de 3,6L. \n";
-print "O preço será de R$ ."$preco_galao. \n";
+print "O preço será de R$ $preco_galao. \n";
 print "\n";
 print "ou \n";
 
     $quantidade_galao2 = $quantidade_galao;
     $quantidade_lata2 = 0;
+    
+ while ($quantidade_galao2 >= 5) {
 
-if ($quantidade_galao2 >= 5) {
+    $quantidade_lata2 = $quantidade_lata2 + 1;
+    $quantidade_galao2 = $quantidade_galao2 - 5;
 
-    $quantidade_lata2 = ceil($quantidade_galao2/5);
-    $quantidade_galao2 = ceil(quantidade_galao2%5);
-}
+ }
       $total_a_pagar = ($quantidade_galao2*25)+($quantidade_lata2*80);
-
 print "Você precisará de $quantidade_galao2 galão(ões) e $quantidade_lata2 lata(s) de tinta. \n";
 print "O total a pagar será de " .'R$' ."$total_a_pagar";
+
